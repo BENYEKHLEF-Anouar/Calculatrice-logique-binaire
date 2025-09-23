483fe8d8acffd3170cdcee06033b5750ebea6121
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-09-23 09:47:00 +0100

    Finished live coding prototype

--- Live-coding-prototype/src/formatterTrait.php
+++ Live-coding-prototype/src/formatterTrait.php
@@ -3,7 +3,10 @@
 namespace App;
 
 trait FormatterTrait {
+    
     public function format(string $label, string|int $value): string {
+
         return str_pad($label, 13, " ", STR_PAD_RIGHT) . ": " . $value . PHP_EOL;
+
     }
 }
